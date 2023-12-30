fc36e23b267cf4ef56a04223848e4abee29bd680
Dwsy <dwsycode@gmail.com>
2023-12-30 13:53:35 +0800

    create

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use hyper::service::{make_service_fn, service_fn};
 use tokio::fs::read;
 use async_process::Command;
 use base64;
-use std::process::exit;
+
 use std::error::Error;
 use hyper::header::CONTENT_TYPE;
 
@@ -13,8 +13,9 @@ async fn get_desktop_picture() -> Result<String, Box<dyn Error + Send + Sync>> {
         .arg("tell application \"Finder\" to get POSIX path of (get desktop picture as alias)")
         .output()
         .await?;
-
-    Ok(String::from_utf8(output.stdout)?.trim().to_string())
+    let path  = String::from_utf8(output.stdout)?.trim().to_string();
+    println!("{}", path);
+    Ok(path)
 }
 
 async fn to_base64(file_path: String) -> Result<String, Box<dyn Error + Send + Sync>> {
@@ -33,7 +34,36 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Err
                             Ok(Response::builder()
                                 .status(StatusCode::OK)
                                 .header(CONTENT_TYPE, "text/plain")
+                                .header("Access-Control-Allow-Origin", "*")
                                 .body(Body::from(base64))
+                                .unwrap())
+                        },
+                        Err(_) => {
+                            Ok(Response::builder()
+                                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                                .body(Body::from("Failed to read file"))
+                                .unwrap())
+                        }
+                    }
+                },
+                Err(_) => {
+                    Ok(Response::builder()
+                        .status(StatusCode::INTERNAL_SERVER_ERROR)
+                        .body(Body::from("Failed to get desktop picture"))
+                        .unwrap())
+                }
+            }
+        },
+        "/get_desktop_picture" => {
+            match get_desktop_picture().await {
+                Ok(file_path) => {
+                    match tokio::fs::read(&file_path).await {
+                        Ok(data) => {
+                            Ok(Response::builder()
+                                .status(StatusCode::OK)
+                                .header(CONTENT_TYPE, "image/jpg") // 根据你的桌面图片的真实格式进行设置，比如"image/png"
+                                .header("Access-Control-Allow-Origin", "*")
+                                .body(Body::from(data))
                                 .unwrap())
                         },
                         Err(_) => {
